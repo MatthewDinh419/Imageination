aaad3a4f26b6ea9a3a5dd2ad31ee0fd6d2b84c57
MatthewDinh419 <32109652+MatthewDinh419@users.noreply.github.com>
2019-11-24 18:48:05 -0700

    Added image uploading

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
-
+import os
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("homepage.html")
 
+app.config["IMAGE_UPLOADS"] = os.path.join('static','uploads')
+
+
+@app.route('/upload', methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+            print(image)
+            return render_template("homepage.html", filename=os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+    return render_template("homepage.html")
 #start flask app
 app.run(host="0.0.0.0", port=5000)
