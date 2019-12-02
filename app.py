08e542a766543ba5d9a61d4b7bf16701cb7a0c60
MatthewDinh419 <32109652+MatthewDinh419@users.noreply.github.com>
2019-12-02 15:22:41 -0700

    Added image history link

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 import os
 
-@app.route('/')
-def hello_world():
-    return render_template("homepage.html")
-
 app.config["IMAGE_UPLOADS"] = os.path.join('static','uploads')
 
+@app.route('/')
+def homepage():
+    return render_template("homepage.html")
 
 @app.route('/upload', methods=["GET", "POST"])
 def upload():
@@ -18,5 +17,10 @@ def upload():
             print(image)
             return render_template("homepage.html", filename=os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
     return render_template("homepage.html")
+
+@app.route('/history', methods=["GET"])
+def history():
+    return render_template("history.html")
+
 #start flask app
 app.run(host="0.0.0.0", port=5000)
