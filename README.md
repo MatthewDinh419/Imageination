f52639deb8924d5186f1f1ee17c99b4cc9e673fa
MatthewDinh419 <32109652+MatthewDinh419@users.noreply.github.com>
2019-12-07 16:23:36 -0700

    Finished database logic, and front-end development

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Imageination
+
+A website where a user will upload an image and then a caption that describes what is going on in the image will be returned.
+
+The application utilizes flask, a python web framework, for server deployment as well as materialize for the front-end.
+
+Components:
+
+    -Redis Database
+    -Key/Value Pairs
+    -Virtual Machine deployment using GCP
+    -Restful API
+    -Tensorflow
+Requirements to be installed:
+
+    -Redis v3.3.11
+    -Flask v1.1.1
+    -Tensorflow v2.0
+How to Run:
+
+    1) Install the dependencies listed above
+    2) Run app.py
--- app.py
+++ app.py
@@ -1,26 +1,45 @@
-from flask import Flask, render_template, request, redirect
-app = Flask(__name__)
+from flask import Flask, render_template, request
 import os
+import redis
+app = Flask(__name__)
 
+#Setup base directory for images
 app.config["IMAGE_UPLOADS"] = os.path.join('static','uploads')
+
+#Setup redis database
+hostname = "localhost"
+redis_history = redis.Redis(host=hostname, db=1)
 
 @app.route('/')
 def homepage():
-    return render_template("homepage.html")
+    return render_template("homepage.html", filename_dict=None)
 
 @app.route('/upload', methods=["GET", "POST"])
 def upload():
     if request.method == "POST":
         if request.files:
             image = request.files["image"]
-            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
-            print(image)
-            return render_template("homepage.html", filename=os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+            img_path = os.path.join(app.config["IMAGE_UPLOADS"], image.filename)
+            image.save(img_path)
+
+            caption = "INSERT CAPTION HERE"
+            redis_history.set(image.filename, caption)
+            filename_caption_dict = {}
+            filename_caption_dict[image.filename] = caption
+
+            return render_template("homepage.html", filename_dict=filename_caption_dict)
     return render_template("homepage.html")
 
 @app.route('/history', methods=["GET"])
 def history():
-    return render_template("history.html")
+    filename_caption_dict = {}
+    for key in redis_history.keys("*"):
+        print(key.decode('utf-8'))
+        print(redis_history.get(key))
+        print("========================")
+        filename_caption_dict[key.decode('utf-8')] = redis_history.get(key).decode('utf-8')
+
+    return render_template("history.html", redis_json=filename_caption_dict)
 
 #start flask app
 app.run(host="0.0.0.0", port=5000)
