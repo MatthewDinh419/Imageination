3fa0db57c67e018e63e52ad284aa65301ac26bc6
EricEKim <eric.kim@colorado.edu>
2019-12-08 21:34:36 -0700

    fixed startup script for rest

--- GCP/rest-install.sh
+++ GCP/rest-install.sh
@@ -2,6 +2,12 @@
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip git
 sudo pip3 install flask
+sudo pip3 install -U virtualenv
+virtualenv --system-site-packages -p python3 ./venv
+source ./venv/bin/activate
+sudo pip3 install --upgrade tensorflow
+sudo pip3 install sklearn
+sudo pip3 install tqdm
 sudo apt-get install -y python3 python3-pip python3-pillow python3-openalpr python3-redis
 git clone https://github.com/MatthewDinh419/Imageination.git
 cd Imageination
